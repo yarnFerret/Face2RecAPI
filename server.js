2ddeaef97b531426437286508f808dc394cc72cb
FerretsNook <yarnferret@gmail.com>
2020-06-17 17:43:31 +0200

    bug fix

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => { res.send(database.users) })
+app.get('/', (req, res) => { res.send('it is working') })
 app.post('/signin', (req, res) => { signin.handleSignIn(req, res, db, bcrypt) })
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 // id = id: id <-es6 feature
