b1c90324937e0715ad87151cd394cc0da046efa8
FerretsNook <yarnferret@gmail.com>
2020-06-18 14:51:43 +0200

    fix 3

--- server.js
+++ server.js
@@ -13,9 +13,13 @@ const db = knex({
   client: 'pg',
   connection: {
     connectionString: process.env.DATABASE_URL,
-    ssl: true
+    ssl: {
+      rejectUnauthorized: false
+    }
   }
 });
+
+console.log(process.env.DATABASE_URL);
 
 const app = express();
 
